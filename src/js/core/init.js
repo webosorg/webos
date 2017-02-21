d86f9a6e2eccfde113bccbfbd8464e333afb885a
Suren <surenatoyan93@gmail.com>
2017-02-21 23:22:47 +0400

    Test for webos-bot

--- src/js/core/init.js
+++ src/js/core/init.js
@@ -1,17 +1,19 @@
+//
 import Dispatcher from './dispatcher.js';
 
 import AppQueue from './appQueue.js';
 
 import Process from './process';
 
-let webOs = {}
+let webOs = {};
 
-webOs.dispatcher = new Dispatcher
+webOs.dispatcher = new Dispatcher;
 
-webOs.appQueue = new AppQueue(webOs.dispatcher)
+webOs.appQueue = new AppQueue(webOs.dispatcher);
 
-webOs.process = new Process(webOs.dispatcher)
+webOs.process = new Process(webOs.dispatcher);
 
-export default webOs
+export default webOs;
 
+'It is only for webos-bot testing'
 'It is only for webos-bot testing'
