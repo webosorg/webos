85cfe7ca1bc18ef1e4dab8544f4e1187dfff0e8b
Suren <surenatoyan93@gmail.com>
2017-02-28 01:23:06 +0400

    create simple example for webassembly

--- assembly/demo/demo.js
+++ assembly/demo/demo.js
@@ -1,12 +1,17 @@
 loadWebAssembly('../math.calc/math.calc.wasm')
   .then(instance => {
     let exports = instance.exports; // the exports of that instance
+
     console.log('Log ::: exports', exports);
-    var add = exports._add; // the "add" function (note "_" prefix)
-    var sub = exports._sub; // the "sub" function (note "_" prefix)
-    var dev = exports._dev; // the "dev" function (note "_" prefix)
-    var mul = exports._mul; // the "mul" function (note "_" prefix)
-    
+
+    let add = exports._add; // the "add" function (note "_" prefix)
+
+    let sub = exports._sub; // the "sub" function (note "_" prefix)
+
+    let dev = exports._dev; // the "dev" function (note "_" prefix)
+
+    let mul = exports._mul; // the "mul" function (note "_" prefix)
+
     console.log('Log ::: Result from binary \'add\' ::: ', add(10, 3));
     console.log('Log ::: Result from binary \'sub\' ::: ', sub(10, 3));
     console.log('Log ::: Result from binary \'dev\' ::: ', dev(10, 3));
