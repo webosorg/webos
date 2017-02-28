1212059b453868712f6d1967fc7dae7675c59f81
Suren <surenatoyan93@gmail.com>
2017-02-28 19:19:38 +0400

    add functions in math.calc and add comments

--- assembly/demo/demo.js
+++ assembly/demo/demo.js
@@ -1,20 +1,29 @@
 loadWebAssembly('../math.calc/math.calc.wasm')
-  .then(instance => {
-    let exports = instance.exports; // the exports of that instance
+  .then(
+    instance => {
+      let exports = instance.exports; // the exports of that instance
 
-    console.log('Log ::: exports', exports);
+      console.log('Log ::: exports', exports);
 
-    let add = exports._add; // the "add" function (note "_" prefix)
+      let add = exports._add; // the "add" function (note "_" prefix)
 
-    let sub = exports._sub; // the "sub" function (note "_" prefix)
+      let sub = exports._sub; // the "sub" function (note "_" prefix)
 
-    let dev = exports._dev; // the "dev" function (note "_" prefix)
+      let dev = exports._dev; // the "dev" function (note "_" prefix)
 
-    let mul = exports._mul; // the "mul" function (note "_" prefix)
+      let mul = exports._mul; // the "mul" function (note "_" prefix)
 
-    console.log('Log ::: Result from binary \'add\' ::: ', add(10, 3));
-    console.log('Log ::: Result from binary \'sub\' ::: ', sub(10, 3));
-    console.log('Log ::: Result from binary \'dev\' ::: ', dev(10, 3));
-    console.log('Log ::: Result from binary \'mul\' ::: ', mul(10, 3));
-  }
-);
+      let fib = exports._fib; // the "fib" function (note "_" prefix)
+
+      console.log('Log ::: Result from binary \'add\' ::: ', add(10, 3));
+      console.log('Log ::: Result from binary \'sub\' ::: ', sub(10, 3));
+      console.log('Log ::: Result from binary \'dev\' ::: ', dev(10, 3));
+      console.log('Log ::: Result from binary \'mul\' ::: ', mul(10, 3));
+      console.log('Log ::: Result from binary \'fib\' ::: ', fib(15));
+    }
+  )
+  .catch(
+    err => {
+      console.log('Error ::: ', err);
+    }
+  );
--- assembly/math.calc/math.calc.c
+++ assembly/math.calc/math.calc.c
@@ -1,15 +1,73 @@
+/**
+ * In this module implemented math functions 
+ * in C progrmming language for WebOs calculator.
+ * This source will be compiled in .wasm
+ * for use in browser by WebAssembly teqnalogies
+ */
+
+/**
+ * Returns a sum of two arguments.
+ *
+ * @param { int } a
+ * @param { int } b
+ * @return { int } sum of two arguments
+ */
 int add(int a, int b) {
   return a + b;
 }
 
+/**
+ * Return a division of two arguments.
+ *
+ * @param { int } a
+ * @param { int } b
+ * @return { int } division of two arguments
+ */
 int dev(int a, int b) {
   return a / b;
 }
 
+/**
+ * Return a subtraction of two arguments.
+ *
+ * @param { int } a
+ * @param { int } b
+ * @return { int } subtraction of two argument
+ */
 int sub(int a, int b) {
   return a - b;
 }
 
+/**
+ * Return a multiplication of two arguments.
+ *
+ * @param { int } a
+ * @param { int } b
+ * @return { int } multiplication of two argument
+ */
 int mul(int a, int b) {
   return a * b;
+}
+
+/**
+ * Return a nth element of fibonacci, using recursion with memoization.
+ * 
+ * @hash# { int[] } fins
+ * @param { int } n
+ * @return { int } nth element of fibonacci
+ */
+int fibs[10000];
+ 
+int fib(int n) {
+  if (n < 2) {
+   return n;
+  } else {
+    if (fibs[n - 1] == 0) {
+      fibs[n - 1] = fib(n - 1);
+   }
+   if (fibs[n - 2] == 0) {
+     fibs[n - 2] = fib(n - 2);
+    }
+   return fibs[n - 2] + fibs[n - 1];
+  }
 }
