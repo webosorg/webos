6caa333918644c0b8acaa93d25b3418fbfb4d91d
SurenAt <suren.ksdm@gmail.com>
2017-03-14 09:42:41 +0400

    Fix bug in service worker

--- src/init.js
+++ src/init.js
@@ -1,15 +1,15 @@
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register(
-//       'service.worker.js'
-//     )
-//     .then(
-//       function (res) {
-//         console.log('res : ', res);
-//       }
-//     )
-//     .catch(
-//       function(err) {
-//         throw new Error('System error ::: ServiceWorker error ::: ', err);
-//       }
-//     );
-// };
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register(
+    'service.worker.js'
+  )
+  .then(
+    function (res) {
+      console.log('res : ', res);
+    }
+  )
+  .catch(
+    function(err) {
+      throw new Error('System error ::: ServiceWorker error ::: ', err);
+    }
+  );
+};
